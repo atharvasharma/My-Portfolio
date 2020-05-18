1ab8883a3ec2bb2e444ed02ebc065d2940498b46
atharvasharma <atharvasjp1@gmail.com>
2020-05-18 22:56:11 +0530

    Updates

---
+++ precache-manifest.af9fc3398ae88166bba6ffb4c10daf7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6171f5041edc01f8019b",
+    "url": "/portfolio/static/css/main.7ea6b8aa.chunk.css"
+  },
+  {
+    "revision": "6171f5041edc01f8019b",
+    "url": "/portfolio/static/js/main.6171f504.chunk.js"
+  },
+  {
+    "revision": "6cbc3df165ce5adaa8f4",
+    "url": "/portfolio/static/js/1.6cbc3df1.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "66b0ba844a762c782e0d87334403e254",
+    "url": "/portfolio/index.html"
+  }
+];
